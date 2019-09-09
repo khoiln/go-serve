65a8ae4d1a6ebffef94aaea2e0860825492b5139
khoi <khoiracle@gmail.com>
2019-09-09 17:38:21 +0700

    No cache

--- main.go
+++ main.go
@@ -2,31 +2,32 @@ package main
 
 import (
 	"flag"
-	"net/http"
 	"log"
-	"io"
+	"net/http"
 	"os"
 )
 
-type loggingMiddleware struct {
-	writer io.Writer
-	next http.Handler
-	}
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		os.Stdout.Write([]byte(r.RequestURI + "\n"))
+		next.ServeHTTP(w, r)
+	})
+}
 
-func (h *loggingMiddleware) ServeHTTP(w http.ResponseWriter,r *http.Request) {
-	h.writer.Write([]byte(r.RequestURI + "\n"))
-	h.next.ServeHTTP(w, r)
+func noCacheMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, post-check=0, pre-check=0")
+		next.ServeHTTP(w, r)
+	})
 }
 
 func main() {
-	port := flag.String("p", "8080","port to serve")
+	port := flag.String("p", "8080", "port to serve")
 	dir := flag.String("d", ".", "directory to serve")
 	flag.Parse()
+	fileServer := http.FileServer(http.Dir(*dir))
 
-	http.Handle("/", &loggingMiddleware{
-		os.Stdout,
-		http.FileServer(http.Dir(*dir)),
-	})
+	http.Handle("/", noCacheMiddleware(loggingMiddleware(fileServer)))
 
 	log.Printf("Serving %s on HTTP port: %s\n", *dir, *port)
 	log.Fatal(http.ListenAndServe(":"+*port, nil))
